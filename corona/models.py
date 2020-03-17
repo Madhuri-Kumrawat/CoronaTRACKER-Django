7307fd96b5ebb5d0393385067872773acb7a5dc6
Madhuri Kumrawat <mad.y.12794@gmail.com>
2020-03-17 16:11:39 -0300

    Finally Data displyed from Database

--- corona/models.py
+++ corona/models.py
@@ -2,6 +2,13 @@ from django.db import models
 from django.db.models import Sum,QuerySet,Subquery,Count
 
 # Create your models here.
+class country(models.Model):
+    city=models.CharField(max_length=100,null=True)
+    state = models.CharField(max_length=100,null=True)
+    country=models.CharField(max_length=100)
+    lat=models.DecimalField(max_length=100,decimal_places=4,max_digits=7)
+    lng=models.DecimalField(max_length=100,decimal_places=4,max_digits=7)
+
 class corona_data(models.Model):
     state=models.CharField(max_length=100)
     country=models.CharField(max_length=100)
@@ -10,8 +17,12 @@ class corona_data(models.Model):
     updated_at=models.DateTimeField('Updated Date',null=True)
 
     def all_countries(self):
-        return corona_data.objects.all()
+        return country.objects.all()
 
     def all_countries_confirmed(self):
         return corona_data.objects.values('country').annotate(total_cases=Sum('confirmed'))
-
+    def map_data(self):
+        return corona_data.objects.raw('SELECT corona_corona_data.id, corona_corona_data.country,corona_corona_data.state,corona_country.lat,corona_country.lng,sum(confirmed) as total_cases  FROM corona_corona_data '
+                                       ' JOIN corona_country ON corona_corona_data.country=corona_country.country AND corona_corona_data.state=corona_country.state'
+                                       ' GROUP BY corona_corona_data.country,corona_corona_data.state'
+                                       ' HAVING sum(confirmed)>0')
--- corona/views.py
+++ corona/views.py
@@ -5,6 +5,7 @@ import plotly.graph_objects as go
 import chart_studio
 import chart_studio.plotly as py
 import plotly.express as px
+import pprint
 
 from django.shortcuts import get_object_or_404
 
@@ -24,11 +25,65 @@ class IndexView(generic.ListView):
         #     y=[16, 5, 11, 9]
         # )
         # data = [trace0, trace1]
-        df = px.data.gapminder().query("year == 2007")
-        fig = px.scatter_geo(df, locations="iso_alpha",
-                             size="pop",  # size of markers, "pop" is one of the columns of gapminder
-                             )
+        # df = px.data.gapminder().query("year == 2007")
+        # fig = px.scatter_geo(df, locations="iso_alpha",
+        #                      size="pop",  # size of markers, "pop" is one of the columns of gapminder
+        #                      )
+        limits = [(0, 2), (3, 10), (11, 20), (21, 50), (50, 3000)]
+        colors = ["rgb(0,116,217)", "rgb(255,65,54)", "rgb(133,20,75)", "rgb(255,133,27)", "lightgrey"]
+        cities = []
+        scale = 500
+        maps_data=corona_data.map_data(self)
+        for data in maps_data:
+            country_raw_text = '{0}'.format(data.total_cases) +' <br> '+data.state + '<br> ' + (data.country )
+
+            city = go.Scattergeo(
+                locationmode='country names',
+                lon=[data.lng],
+                lat=[data.lat],
+                text=country_raw_text,
+                marker=go.scattergeo.Marker(
+                    size=data.total_cases / scale,
+                    color=colors[3],
+                    line=go.scattergeo.marker.Line(
+                        width=0.5, color='rgb(40,40,40)'
+                    ),
+                    sizemode='area'
+                ),
+                name='{0}'.format(data.total_cases))
+            cities.append(city)
+
+        layout = go.Layout(
+            title=go.layout.Title(
+                text='Corona Effect'
+            ),
+            showlegend=True,
+            geo=go.layout.Geo(
+                scope='world',
+                projection=go.layout.geo.Projection(
+                    type='natural earth'
+                ),
+                showland=True,
+                landcolor='rgb(217, 217, 217)',
+                subunitwidth=1,
+                countrywidth=1,
+                subunitcolor="rgb(255, 255, 255)",
+                countrycolor="rgb(255, 255, 255)"
+            )
+        )
+
+        fig = go.Figure(data=cities, layout=layout)
         return py.plot(fig, filename='bubble_map',auto_open=False,sharing='public')
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        context['page_title'] = 'Corona Details'
+
+        context['maps_data'] =corona_data.map_data(self)
+
+        return context
+
 
 class DetailView(generic.DetailView):
     template_name = 'corona/detail.html'
